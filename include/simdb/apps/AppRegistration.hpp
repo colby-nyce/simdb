24df1db89a804ab9cd1e7a7e88feb77d915f194d
Colby Nyce <iheartridleyman@gmail.com>
2025-06-07 07:31:21 -0500

    Add comments to AppRegistration

--- include/simdb/apps/AppRegistration.hpp
+++ include/simdb/apps/AppRegistration.hpp
@@ -7,11 +7,38 @@
 
 #include "simdb/sqlite/DatabaseManager.hpp"
 
+/// Aside from its core SQLite functionality, SimDB provides a framework for
+/// creating "apps" which get selectively enabled based on your simulation
+/// configuration (e.g. command line options, config file, etc).
+///
+///   - Create your own data/metadata schema tables just for your app
+///   - Use SimDB utilities to build async compression/DB pipelines
+///
+/// Example applications:
+///
+///   - Logger that records simulation events
+///   - Profiler that tracks performance metrics
+///   - Pipeline collector
+///   - SQLite-to-HDF5 converter
+///   - Backend for a live data visualization GUI / web interface
+///
+/// Since SimDB is designed to be simulator-agnostic, apps also provide
+/// a variety of hooks that allow you to run code at different stages of
+/// the simulation lifecycle and to ensure that all apps in your simulator
+/// are initialized and run in a consistent manner.
+///
+///   - appendSchema:  first hook after command line args / config files are parsed
+///   - preInit:       called before the simulation starts, after command line parsing
+///   - preSim:        called before the simulation loop starts
+///   - postSim:       called after the simulation loop ends
+///   - teardown:      called after the simulation ends, for resource cleanup tasks
+
 namespace simdb
 {
 
 class DatabaseManager;
 
+/// Base class for SimDB applications.
 class App
 {
 public:
@@ -52,6 +79,11 @@ public:
     }
 };
 
+/// @brief Singleton class that manages all SimDB applications.
+///
+/// This class is responsible for registering, enabling, instantiating,
+/// and managing the lifecycle of all SimDB applications running in a
+/// simulation.
 class AppManager
 {
 public:
@@ -61,12 +93,31 @@ public:
         return instance;
     }
 
+    /// Register an app as early as possible (doesn't create it yet) Register
+    /// your app by adding this macro to a translation unit at file scope:
+    ///
+    ///   class MyApp : public simdb::App { ... };
+    ///   REGISTER_SIMDB_APPLICATION(MyApp); 
     template <typename AppT>
     void registerApp()
     {
         app_factories_[AppT::NAME] = std::make_unique<AppFactory<AppT>>();
     }
 
+    /// After parsing command line arguments or configuration files,
+    /// enable an app by its name. This will allow the app to be instantiated
+    /// and run during the simulation lifecycle.
+    ///
+    ///   class MyApp : public simdb::App
+    ///   {
+    ///   public:
+    ///       static constexpr const char* NAME = "MyApp";
+    ///       // ...
+    ///   };
+    ///
+    ///   // Somewhere in your main function or config parsing code (meaning,
+    ///   // after command line args are parsed and very early in the simulation):
+    ///   simdb::AppManager::getInstance().enableApp(MyApp::NAME);
     void enableApp(const std::string& app_name)
     {
         if (!enabled_apps_.insert(app_name).second)
@@ -75,16 +126,19 @@ public:
         }
     }
 
+    /// Check if your app is enabled (might not be instantiated yet).
     bool enabled(const std::string& app_name) const
     {
         return enabled_apps_.find(app_name) != enabled_apps_.end();
     }
 
+    /// Check if your app is instantiated (enabled and created).
     bool instantiated(const std::string& app_name) const
     {
         return apps_.find(app_name) != apps_.end();
     }
 
+    /// Call after command line args and config files are parsed.
     bool createEnabledApps(DatabaseManager* db_mgr)
     {
         for (const auto& app_name : enabled_apps_)
@@ -96,6 +150,7 @@ public:
         return !apps_.empty();
     }
 
+    /// Get an instantiated app. Throws if not found.
     template <typename AppT>
     AppT* getApp(bool must_exist = true)
     {
@@ -120,6 +175,7 @@ public:
         return nullptr;
     }
 
+    /// Create app-specific schemas for the instantiated apps.
     void createSchemas(DatabaseManager* db_mgr)
     {
         db_mgr->safeTransaction(
@@ -146,6 +202,7 @@ public:
             });
     }
 
+    /// Call this after command line args and config files are parsed.
     void preInit(DatabaseManager* db_mgr, int argc, char** argv)
     {
         db_mgr->safeTransaction(
@@ -159,6 +216,8 @@ public:
             });
     }
 
+    /// Call this before the simulation loop starts, but after
+    /// the simulator is fully initialized.
     void preSim(DatabaseManager* db_mgr)
     {
         db_mgr->safeTransaction(
@@ -172,6 +231,7 @@ public:
             });
     }
 
+    /// Call this after the simulation loop ends for post-processing tasks.
     void postSim(DatabaseManager* db_mgr)
     {
         db_mgr->safeTransaction(
@@ -185,6 +245,9 @@ public:
             });
     }
 
+    /// Call this after the simulation ends for resource cleanup tasks
+    /// such as closing files, releasing memory, flushing/shutting down
+    /// background threads, etc.
     void teardown(DatabaseManager* db_mgr)
     {
         for (const auto& [name, app] : apps_)
@@ -193,6 +256,9 @@ public:
         }
     }
 
+    /// Delete all instantiated apps. This may be needed since AppManager
+    /// is a singleton and your simulator might want to call app destructors
+    /// before the AppManager itself is destroyed on program exit.
     void deleteApps()
     {
         apps_.clear();
@@ -202,8 +268,14 @@ public:
 
 private:
     AppManager() = default;
+
+    /// Registered app factories.
     std::map<std::string, std::unique_ptr<AppFactoryBase>> app_factories_;
+
+    /// Instantiated apps (implicitly enabled).
     std::map<std::string, std::unique_ptr<App>> apps_;
+
+    /// Enabled apps (may or may not be instantiated).
     std::set<std::string> enabled_apps_;
 };
 
