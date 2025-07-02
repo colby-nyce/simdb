449cf2d8205b0077d07bd3791a12be7c5ace9f29
Colby Nyce <iheartridleyman@gmail.com>
2025-07-02 15:24:14 -0500

    Touchups

--- include/simdb/apps/AppManager.hpp
+++ include/simdb/apps/AppManager.hpp
@@ -305,7 +305,7 @@ public:
         }
 
         std::cout << "************ Shutting down pipelines for all SimDB apps on database: "
-                  << db_mgr->getDatabaseFilePath() << "\n\n";
+                  << db_mgr->getDatabaseFilePath() << " ************\n\n";
 
         auto it = non_db_threads_.find(db_mgr);
         if (it != non_db_threads_.end())
