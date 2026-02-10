c7b1e6b9eb76fb56ad9ac03c9c5e169e24e955a0
Brett Dutro <bdutro@mips.com>
2026-02-10 12:15:25 -0800

    Move AppManager::getEnabledApps_ to a .cpp file to prevent multiple definitions

---
+++ src/AppManager.cpp
@@ -0,0 +1,10 @@
+#include "simdb/apps/AppManager.hpp"
+
+namespace simdb
+{
+    std::map<std::string, size_t> & AppManager::getEnabledApps_()
+    {
+        static std::map<std::string, size_t> enabled_apps;
+        return enabled_apps;
+    }
+}
