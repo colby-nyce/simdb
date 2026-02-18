442590b94189a415840dd51f967210c5a58566c4
Colby Nyce <cnyce@mips.com>
2026-02-18 06:55:36 -0800

    Move to clang-format-19

--- .github/actions/clang-format-check.sh
+++ .github/actions/clang-format-check.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 FILES=$(git ls-files '*.cpp' '*.cc' '*.cxx' '*.h' '*.hpp')
 
-clang-format-17 -i $FILES
+clang-format-19 -i $FILES
 
 if ! git diff --quiet; then
   echo "clang-format violations detected"
